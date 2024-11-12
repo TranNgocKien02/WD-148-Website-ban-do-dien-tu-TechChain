70264c191e9838053a4ad203b3388193f864cc73
phamtuandatph38223 <phamtuandat1503@gmail.com>
2024-11-12 10:15:29 +0700

    tuong tac

--- database/migrations/2024_11_12_030915_create_likes_table.php
+++ database/migrations/2024_11_12_030915_create_likes_table.php
@@ -1,27 +1,21 @@
 <?php
 
-use App\Models\SanPham;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(SanPham::class)->constrained();
             $table->foreignIdFor(User::class)->constrained();
-            // $table->string('ma_san_pham')->unique();
-            $table->string('hinh_anh');
-            $table->string('like');
-            $table->text('content');
-            // $table->boolean('chapnhan')->default(false);
+            $table->foreignIdFor(Comment::class)->constrained();
             $table->timestamps();
         });
     }
@@ -31,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('likes');
     }
 };
